9859088f112698560d67576e88c43b2692a58909
mateomaza <1mateomaza@gmail.com>
2022-06-20 14:50:27 -0500

    solved a bug :p
    I solved it with an asynchronous function before

--- js/site.js
+++ js/site.js
@@ -1,41 +1,83 @@
 // We call the HTML elements we need
+
 const btn = document.getElementById("button")
 const display = document.getElementById("display")
 
-// Our only function :)
-function result() {
 
-  // Controller
-  // We store the values of the two inputs in variables
+// Controller
+
+function getValues() {
+
+  // We store the input values in variables
   let sValue = document.getElementById("sValue").value
   let eValue = document.getElementById("eValue").value
 
-  // We convert those values into integers
+  // We convert those values into Number() objects
   sValue = Number(sValue);
   eValue = Number(eValue);
 
-  // Logic
-  // We test if those values are "0-100" integers or not, and make sure they're not undefined
-  if (Number.isInteger(sValue) && Number.isInteger(eValue) && sValue >= 0 && sValue < 100 && eValue > 0 && eValue <= 100 
-    && typeof sValue !== undefined && typeof eValue !== undefined) {
-    for (i = sValue; i <= eValue; i++) {
-      // Display - We create one tr and one td in every iteration, then we append them accordingly
-      var row = document.createElement("tr")
-      var data = document.createElement("td")
-      display.appendChild(row)
-      row.appendChild(data)
-      data.innerHTML = i
-      // A little more logic (I'll create 3 functions next time :s)
-      if (i % 2 == 0) {
-        data.className = "even"
-      } else {
-        data.className = "odd"
-      }
-    }
+  // We test if those values are integers from 0 to 100 or not (and make sure they're not undefined)
+  if (Number.isInteger(sValue) && Number.isInteger(eValue) && sValue >= 0 && sValue < 100 && eValue > 0 && eValue <= 100 &&
+    typeof sValue !== undefined && typeof eValue !== undefined) {
+
+    // We call the functions to generate "nums" and display it
+    let nums = generateNums(sValue, eValue)
+    displayNums(nums)
+
   } else {
     alert("Start Value must be an integer between 0 & 99, End Value must be an integer between 1 & 100")
   }
 }
 
-//We add a click event handler to a button, so our project start when clicked
-btn.addEventListener("click", result)
+
+// Logic
+
+function generateNums(startValue, endValue) {
+
+  // Variable to store all our numbers generated
+  let numbers = [];
+
+  // We append every number in the sValue:eValue range to the variable "numbers"
+  for (let i = startValue; i <= endValue; i++) {
+    numbers.push(i)
+  }
+
+  // We return "numbers" so our variables "num" work in the controller
+  return numbers
+}
+
+
+// Display
+
+function displayNums(numbers){
+   
+  // Variable where we'll store all rows generated
+  let templateRows = "";
+
+  for (let i = 0; i < numbers.length; i++) {
+      
+      // Variable for classes, so we make even numbers bold with CSS
+      let className = "";
+      
+      // We pull out each individual number each time we iterate through the loop
+      let number = numbers[i];
+      
+      // We set an "if statement" to add a different class to even and odd numbers
+      if (number % 2 == 0){
+          className = "even"
+      } else {
+          className = "odd"
+      }
+
+      // We add each row with "template literals"
+      templateRows += `<tr><td class="${className}" >${number}</td></tr>`
+  }
+  
+  // We add all the content for display to the tbody tag
+  display.innerHTML = templateRows
+}
+
+
+//The click event handler to a button, so our project starts when clicked
+
+btn.addEventListener("click", getValues)
