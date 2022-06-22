462e728e771b6e82d906e2b44b26353f7e8a4ffb
mateomaza <1mateomaza@gmail.com>
2022-06-22 15:42:29 -0500

    solved a little bug
    empty sValue still worked

--- js/site.js
+++ js/site.js
@@ -12,21 +12,25 @@ function getValues() {
   let sValue = document.getElementById("sValue").value
   let eValue = document.getElementById("eValue").value
 
-  // We convert those values into Number() objects
-  sValue = Number(sValue);
-  eValue = Number(eValue);
+  // We make sure both values aren't empty (empty inputs pass as empty arrays)
+  // Technically, the second statements isn't necessary, as eValue = 0 won't work, so just for comprehension
+  if (sValue !== '' && typeof eValue !== '') {
 
-  // We test if those values are integers from 0 to 100 or not (and make sure they're not undefined)
-  if (Number.isInteger(sValue) && Number.isInteger(eValue) && sValue >= 0 && sValue < 100 && eValue > 0 && eValue <= 100 &&
-    typeof sValue !== undefined && typeof eValue !== undefined) {
+    // We convert those values into Number() objects
+    sValue = Number(sValue);
+    eValue = Number(eValue);
 
-    // We call the functions to generate "nums" and display it
-    let nums = generateNums(sValue, eValue)
-    displayNums(nums)
+    // We test if those values are integers from 0 to 100 or not (and make sure they're not undefined)
+    if (Number.isInteger(sValue) && Number.isInteger(eValue) && sValue >= 0 && sValue < 100 && eValue > 0 && eValue <= 100) {
 
-  } else {
-    alert("Start Value must be an integer between 0 & 99, End Value must be an integer between 1 & 100")
-  }
+      // We call the functions to generate "nums" and display it
+      let nums = generateNums(sValue, eValue)
+      displayNums(nums)
+
+    } else {
+      alert("Start Value must be an integer between 0 & 99, End Value must be an integer between 1 & 100")
+    }
+  } else {alert("Start Value must be an integer between 0 & 99, End Value must be an integer between 1 & 100")}
 }
 
 
@@ -49,30 +53,30 @@ function generateNums(startValue, endValue) {
 
 // Display
 
-function displayNums(numbers){
-   
+function displayNums(numbers) {
+
   // Variable where we'll store all rows generated
   let templateRows = "";
 
   for (let i = 0; i < numbers.length; i++) {
-      
-      // Variable for classes, so we make even numbers bold with CSS
-      let className = "";
-      
-      // We pull out each individual number each time we iterate through the loop
-      let number = numbers[i];
-      
-      // We set an "if statement" to add a different class to even and odd numbers
-      if (number % 2 == 0){
-          className = "even"
-      } else {
-          className = "odd"
-      }
 
-      // We add each row with "template literals"
-      templateRows += `<tr><td class="${className}" >${number}</td></tr>`
+    // Variable for classes, so we make even numbers bold with CSS
+    let className = "";
+
+    // We pull out each individual number each time we iterate through the loop
+    let number = numbers[i];
+
+    // We set an "if statement" to add a different class to even and odd numbers
+    if (number % 2 == 0) {
+      className = "even"
+    } else {
+      className = "odd"
+    }
+
+    // We add each row with "template literals"
+    templateRows += `<tr><td class="${className}" >${number}</td></tr>`
   }
-  
+
   // We add all the content for display to the tbody tag
   display.innerHTML = templateRows
 }
